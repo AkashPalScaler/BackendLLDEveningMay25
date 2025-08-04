c319b802036e5378dd3a1c1b299049cbcb074920
prajwal kulkarni <kulkarniprajwal.01@gmail.com>
2025-08-04 22:53:38 +0530

    Add diagonal and anti-diagonal winning strategy and update bot difficulty selection

--- src/TicTacToe/Client.java
+++ src/TicTacToe/Client.java
@@ -5,13 +5,29 @@ import TicTacToe.models.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
         GameController gameController = new GameController();
 
         Player akash = new HumanPlayer("Akash", PlayerType.HUMAN, 'X');
-        Player botty = new BotPlayer("Botty", PlayerType.BOT, 'O', BotDifficultyLevel.EASY);
+        BotDifficultyLevel botDifficultyLevel;
+        do {
+            System.out.println("Choose 1 for Easy Bot, 2 for Medium Bot: ");
+            String difficulty = new Scanner(System.in).nextLine();
+            if (difficulty.equals("1")) {
+                botDifficultyLevel = BotDifficultyLevel.EASY;
+                break;
+            } else if (difficulty.equals("2")) {
+                botDifficultyLevel = BotDifficultyLevel.MEDIUM;
+                break;
+            } else {
+                System.out.println("Invalid choice, please try again.");
+            }
+        } while (true);
+
+        Player botty = new BotPlayer("Botty", PlayerType.BOT, 'O', botDifficultyLevel);
 
         List<Player> players = new ArrayList<>();
         players.add(akash);
@@ -20,6 +36,8 @@ public class Client {
         List<WinningStrategyType> winningStrategyTypes = new ArrayList<>();
         winningStrategyTypes.add(WinningStrategyType.COL);
         winningStrategyTypes.add(WinningStrategyType.ROW);
+        winningStrategyTypes.add(WinningStrategyType.DIAGONAL);
+        winningStrategyTypes.add(WinningStrategyType.ANTI_DIAGONAL);
 
 
         Game game1 = gameController.startGame(3, players, winningStrategyTypes);
